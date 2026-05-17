a072643b2cae22bc09fce0cd6c89d90f3532775a
OJddJO <95376647+OJddJO@users.noreply.github.com>
2026-05-17 03:11:08 +0100

    Bump

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ python -m venv venv
 source venv/Scripts/activate
 pip install vapoursynth vsrife vsrepo
 vsrepo install ffms2
-pip install torch-tensorrt --extra-index-url https://download.pytorch.org/whl/cu126
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu126
+pip install torch-tensorrt --extra-index-url https://download.pytorch.org/whl/cu126
