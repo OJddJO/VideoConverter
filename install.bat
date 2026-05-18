91b6c634f93192df6e90099033f990c9a18f940d
OJddJO <95376647+OJddJO@users.noreply.github.com>
2026-05-18 21:37:02 +0100

    Bump

--- install.bat
+++ install.bat
@@ -3,5 +3,4 @@ python -m venv venv
 call venv\Scripts\activate.bat
 pip install vapoursynth vsrife vsrepo
 vsrepo install ffms2
-pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu126
-pip install torch-tensorrt --extra-index-url https://download.pytorch.org/whl/cu126
+pip install torch torch-tensorrt torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu126
--- install.ps1
+++ install.ps1
@@ -2,5 +2,4 @@ python -m venv venv
 .\venv\Scripts\Activate.ps1
 pip install vapoursynth vsrife vsrepo
 vsrepo install ffms2
-pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu126
-pip install torch-tensorrt --extra-index-url https://download.pytorch.org/whl/cu126
+pip install torch torch-tensorrt torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu126
